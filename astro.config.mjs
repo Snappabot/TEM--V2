2f65742cf9429a016385e1a1df94100f8274f53e
Snappabot <snappabot@proton.me>
2026-03-05 10:33:21 +0000

    Switch to Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://snappabot.github.io',
+  site: 'https://www.troweledearthmelbourne.com.au',
   base: '/',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
     server: {
